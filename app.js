c25931244f00c945724f137de4f672b5418ce8c5
damsazalibughio <damsazali73@gmail.com>
2021-07-03 21:36:37 +0500

    all files added

---
+++ app.js
@@ -0,0 +1,80 @@
+// chapter 43 - 48 assignments
+
+/* Task 1  Show an alert box on click on a link.
+*/
+
+// method 1 is on index.html
+
+// method 2
+
+// function showAlert(){
+//     alert("Hello world")
+// }
+
+
+
+/* Task 2 Display some Mobile images in browser. On click on an
+image Show the message in alert to user. */
+
+//this task is  on index.html
+
+/* Task  3 Display 10 student records in table and each row should contain a delete 
+button. If you click on a button to delete a record, entire row should be 
+deleted.  */
+
+//  function displayStudent() {
+//     var studentNameArr = ['Jhone', 'Doe', 'Mark', 'James', 'Asad', 'Hafeez', 'Damsaz', 'Ali','Nadir', 'Shoaib' ];
+//     var studentClassArr = ['10', '09', '10', '08', '07', '07', '10', '03','08', '09' ];
+//     var html = '';
+//     var index = 0;
+
+//     while(index < studentNameArr.length){
+//     html +=`<tr id="student${index}"><td>${index}</td> <td>${studentNameArr[index]}</td> <td>${studentClassArr[index]}</td> <td><button onclick='student${index}.remove()' >Delete</button></td></tr>`;
+
+//     document.getElementById("display-student").innerHTML =`<table cellspacing='15px'>
+//                                                     <th>Index</th>
+//                                                     <th>Name</th>
+//                                                     <th>Class</th>
+//                                                     <th>Action</th>                                              
+//                                                          ${html}                                                   
+//                                                     </table>`;
+//     index++;    
+//     }
+    
+    
+//   }
+//   displayStudent()
+
+
+/* Task 4 Display an image in browser. Change the picture on mouseover and set the
+first picture on mouseout. */
+//this task is on index.html
+
+
+/* Task 5 Show a counter in browser. Counter should increase on click on increase 
+button and decrease on click on decrease button. And show updated counter 
+value in browser */
+// var counter = 0;
+// function increase(){
+    
+//     counter++;
+//     document.getElementById('count').innerHTML=counter
+     
+// }
+
+// function decrease(){
+    
+//     counter--;
+//     document.getElementById('count').innerHTML=counter
+   
+// }
+
+// // extra
+
+// function reset(){
+    
+//     counter =0;
+//     document.getElementById('count').innerHTML=counter
+   
+// }
+
